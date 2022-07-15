23f6074e9e667a203693491802155b32dbaefdbe
TJL <56328319+tlinnard4790@users.noreply.github.com>
2022-07-15 16:31:11 -0500

    Update #2 project.py
    Updated source_frame function
        added code to start pdf to email process
    Removed old graph function(def_main)
    Added functions yearly &, monthly_spending, and all_orders
    Updated category/total_charged code

--- project.py
+++ project.py
@@ -1,24 +1,44 @@
+
+
 from ast import And
 from re import L
 from this import d
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
-import datetime
 import csv
 from math import fsum
+import seaborn as sns
+import emails
+import reports
+import os
 
 
 # This function will print out a dataframe consisting of only the Order_Date, Subtotal, Shipping_Charge, Tax_Charged,
 # and Total_Charged columns. It provides an easier grid to view instead of looking at the 21 columns within Excel.
 # The '$' signs have been removed to make it easier to call the float instead of a string
 
-def clean_frame():
+def source_frame():
+    import pandas as pd
+    import emails
+    import reports
+
     df = pd.read_csv('amazon_spending.csv').fillna('-------')
     df["Total_Charged"] = df["Total_Charged"].str.replace('$','', regex=True).astype(float)
     df["Tax_Charged"] = df["Tax_Charged"].str.replace('$', '', regex=True).astype(float)
-    clean = df[['Order_Date','Subtotal','Shipping_Charge','Tax_Charged','Total_Charged']]
-    return clean
-clean_frame()
+    source = df[['Order_Date','Subtotal','Shipping_Charge','Tax_Charged','Total_Charged']]
+     
+    reports.generate("report.pdf", "YoY Amazon Spending", "Report.", source)
+    sender = "tlinnard4790@gmail.com"
+    receiver = "tlinnard4790@gmail.com".format(os.environ.get('USER'))
+    subject = "YoY Amazon Spending"
+    body = "Hi\n\nI'm sending an attachment regarding your Amazon spending."
+    message = emails.generate(sender, receiver, subject, body, "/New.pdf")
+    #emails.send(message)  
+
+    return source
+     
+source_frame()
 
 
 # Function provides a print return of the total of all spending starting from the origination of the account
@@ -30,19 +50,22 @@ clean_frame()
 
 
 def totals():
-    with open('amazon_spending.csv', 'r') as f:
-        reader = [x for x in csv.DictReader(f)]
 
+    with open('amazon_spending.csv', 'r') as f:
+
+        reader = [x for x in csv.DictReader(f)]
         item_subtotal = fsum([float(x['Subtotal'].strip("$")) for x in reader])
         shipping_total = fsum([float(x['Shipping_Charge'].strip("$")) for x in reader])
         tax_total = fsum([float(x['Tax_Charged'].strip("$")) for x in reader])
         spending_total = fsum([float(x['Total_Charged'].strip("$")) for x in reader])
 
-        print("$",item_subtotal, "spent before shipping costs and taxes")
-        print("$",shipping_total, "spent on shipping charges")
-        print("$",tax_total, "spent on taxes")
-        print("Total amount spent: $",spending_total)
+    print("$",item_subtotal, "spent before shipping costs and taxes")
+    print("$",shipping_total, "spent on shipping charges")
+    print("$",tax_total, "spent on taxes")
+    print("Total amount spent: $",spending_total)
+
 totals()
+
 
 
 #This function pulls 2 columns from the "amazon_items.csv" : the Category and Item_Total
@@ -55,63 +78,82 @@ plt.rcParams["figure.autolayout"] = True
 columns = ["Category", "Item_Total"]
 df = pd.read_csv("amazon_items.csv", usecols=columns)
 print("Contents in csv file:\n", df)
+print(df.groupby(['Category']).sum())
+
 plt.plot(df.Category.astype(str))
 plt.plot(df.Item_Total.astype(str))
 plt.show()
 
 
 
-# df.plot.bar(x='Order Date', y='Total Charged', rot=90, figsize=(30,20))
-# plt.gcf().set_facecolor('white')
+# This function plots a ling graph showcasing my total spending for each year 
 
-#--------------------------------------------------------------------------------------------------------#
+def yearly_spending():
+    import matplotlib.pyplot as plt
+    import seaborn as sns
 
-#file = 'amazon_spending.csv'
+    df = pd.read_csv('amazon_spending.csv')
+    df["Total_Charged"] = df["Total_Charged"].str.replace("$", "", regex=True).astype(float)
+    df["Year"] = pd.DatetimeIndex(df['Order_Date']).year
+    yoy_cost = df.groupby(["Year"], as_index=False).sum()
+    print(yoy_cost)
+    fig, ax = plt.subplots(figsize=(16,7))
 
-# def main():
-#     with open(file) as f:
-#         reader = csv.reader(f)
-#         header_row = next(reader)
+    sns.set_theme()
+    sns.lineplot(x=yoy_cost["Year"], y=yoy_cost["Total_Charged"], palette="mako")
+    sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 5})
+    plt.title("How much did I spend each year?")
+    plt.ylabel("Total Cost ($)")
 
-#         for index, column_header in enumerate(header_row):
-#             if column_header == 'Order_ID':
-#                 orderIndex = index
-#             if column_header == 'Shipment_Date':
-#                 shipIndex = index
-#             if column_header == 'Subtotal':
-#                 subIndex = index
-#             if column_header == 'Tax_Charged':
-#                 taxIndex = index
-#             if column_header == 'Total_Charged':
-#                 totIndex = index
-#              Else:
-#                  pass
+yearly_spending()
 
 
-#         dates, order, sub, tax, tot = [], [], [], [], []
-#         for row in reader:
-#                 current_date = datetime.strptime(row[shipIndex], '%m/%d/%Y')
-#                 order = row[orderIndex]
-#                 sub = row[subIndex]
-#                 tax = row[taxIndex]
-#                 tot = row[totIndex]
-#                 dates.append(current_date)
-#                #order.append(order)
-#                 #sub.append(sub)
-#                 #tax.append(tax)
-#                 #tot.append(tot)
 
-#                 plt.style.use('seaborn')
-#                 fig, ax = plt.subplots()
-#                 ax.plot(dates, order, c='black', alpha=0.5)
-#                 ax.plot(dates, sub, c='blue', alpha=0.5)
-#                 ax.plot(dates, tax, c='red', alpha=0.5)
-#                 ax.plot(dates, tot, c='red', alpha=0.5)
 
-#                 ax.set_title(file)
-#                 ax.set_xlabel('Date')
-#                 fig.autofmt_xdate()
-#                 ax.set_ylabel('Stock Price ($)')
-#                 ax.tick_params(axis='both', which='major')
-# if __name__ == "__main__":
-#    main()
+# This function plots a bar graph showcasing the total spending for each month over the years, so each year for the month of January, etc
+
+def monthly_spending():
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    df = pd.read_csv('amazon_spending.csv')
+    months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    df['Month'] = pd.DatetimeIndex(df['Order_Date']).month
+    df["Total_Charged"] = df["Total_Charged"].str.replace("$", "", regex=True).astype(float)
+    monthly_cost = df.groupby(["Month"], as_index=False).sum()
+    print(monthly_cost)
+    
+
+    sns.set()
+    fig, ax = plt.subplots(figsize=(16,7))
+    sns.set_theme()
+    sns.set_style("darkgrid")
+    sns.barplot(x=monthly_cost["Month"], y=monthly_cost['Total_Charged'], palette="flare")
+    plt.ylabel("Total Cost ($)")
+    plt.title("How much did I spend each month?")
+
+monthly_spending()
+
+
+
+
+
+#This function plots a line graph showcasing the total cost of each order since the creation of my Amazon account
+
+
+def all_orders():
+    import matplotlib.pyplot as plt
+    import seaborn as sns
+
+    df = pd.read_csv('amazon_spending.csv')
+    df["Total_Charged"] = df["Total_Charged"].str.replace("$", "", regex=True).astype(float)
+
+    fig, ax = plt.subplots(figsize=(16,7))
+    sns.lineplot(x=df['Order_Date'], y=df["Total_Charged"], palette="Blues")
+    sns.set_context("notebook", font_scale=1.5, rc={"lines.linewidth": 2.5})
+    plt.ylabel("Total Cost ($)")
+    plt.xlabel('Order Date')
+    plt.xscale('linear')
+    plt.title("Cost of Amazon orders over the years")
+
+all_orders()
